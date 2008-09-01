4c3809b427bd8b2a44fc3e933e0edd7b78d8f65f
Wilkes Joiner <wilkesjoiner@gmail.com>
2008-09-01 10:57:42 -0500

    hacked in Growl support, sloppy

--- djpest.py
+++ djpest.py
@@ -1,26 +1,41 @@
 from pest import Pest
 import os, sys
 
+    
 def exclude_dir(name):
     return name.startswith('.') 
 
 def exclude_file(name):
     return not name.endswith('.py')
 
-def run(module=None): 
+def notify(growl, results):
+    if growl:
+        if results == 0:
+            growl.notify(noteType='pass', title="Tests Pass", description="All tests passed!")
+        else:
+            growl.notify(noteType='fail', title="Tests Failed", description="FAIL!!!")
+
+def run(module, growl): 
     cmd = './manage.py test'
     if module:
         cmd += ' ' + module
     def callback(changes):
-        print changes
-        os.system(cmd)
+        notify(growl, os.system(cmd))
     return callback
 
 def main():
+    try:
+        from Growl import GrowlNotifier
+        gn = GrowlNotifier(applicationName='djpest', notifications=['pass', 'fail'])
+        gn.register()
+    except:
+        gn = None
+    
+    app = None
     if len(sys.argv) == 1:
-        cb = run(sys.argv[1])
-    else:
-        cb = run()
+        app = sys.argv[1]
+
+    cb = run(app, gn)
     Pest(exclude_file=exclude_file, exclude_dir=exclude_dir,callback=cb).start()
     
 
