b97a1526d4489c681630fddfc5dc3514bc3ce2b1
Chuck Collins <chuck.collins@gmail.com>
2009-10-01 12:52:45 -0500

    additional argument is test command to run

---
+++ pest/custom_pest.py
@@ -0,0 +1,11 @@
+import os, pest
+
+class CustomPest(pest.Pest):
+    cmd = ''
+    
+    def set_command(self, cmd):
+        self.cmd = cmd
+        
+    def run_tests(self): 
+        super(CustomPest, self).run_tests()
+        self.notify(self.grade_result(os.system("%s/%s" % (self.root, self.cmd))))
