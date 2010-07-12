db05e9557a0bb145daa5555af2fe38e1c9889383
Chuck Collins <chuck.collins@gmail.com>
2010-07-12 15:12:56 -0500

    added manifest

--- setup.py
+++ setup.py
@@ -3,9 +3,8 @@ from setuptools import setup
 setup(
     name="pest",
     version="1.0",
-    packages = ['pest'],
+    packages=['pest'],
     scripts=['pest/pester'],
-    package_data = {'pest': ['images/*.png'],},
     install_requires=['pyobjc-framework-FSEvents','growl-py'],
     description="Auto tester for python",
     author="Chuck Collins",
